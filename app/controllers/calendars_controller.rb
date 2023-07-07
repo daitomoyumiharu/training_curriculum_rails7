e5cbb30fc346d77944fba9cabd6e155a35c2c81f
daitomoyumiharu <sakanashi0814@gmail.com>
2023-07-07 15:00:07 +0900

    Issue4

--- app/controllers/calendars_controller.rb
+++ app/controllers/calendars_controller.rb
@@ -12,10 +12,13 @@ class CalendarsController < ApplicationController
     redirect_to action: :index
   end
 
+  
+
   private
 
+  
   def plan_params
-    params.require(:calendars).permit(:date, :plan)
+    params.require(:plan).permit(:date, :plan)
   end
 
   def getWeek
